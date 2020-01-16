4a137bfebd8a330b0faddfa064c97d44fa404131
Kolosovsky <aspirius193@gmail.com>
2020-01-16 17:30:05 +0200

    Getting started

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Start')
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = sock.connect_ex(('77.222.153.190',3389))
+if result == 0:
+   print("Port is open")
+else:
+   print("Port is not open")
+sock.close()
